7d3a2b89bde28ab5f19f9217ef78e874d702eb6b
ewewsheep <jack@gtctek.co.uk>
2025-03-30 19:04:28 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function IITSN() {
+    alert('Island in the sun');
+}
+
+function IITSA() {
+    alert('Weezer')
+}
+
+function IITSAN() {
+    alert("The Green Album")
+}
+
+function TSN() {
+    alert('Thriller')
+}
+
+function TAN() {
+    alert('Micheal Jackson')
+}
+
+function TA() {
+    alert('Thriller')
+}
+
+function YSN() {
+    alert('You spin me round')
+}
+
+function YSAN() {
+    alert('DeadOrAlive')
+}
+
+function YSA() {
+    alert('Youthquake')
+}
+
+function BIN() {
+    alert('Beat it')
+}
+
+function BIA() {
+    alert('Micheal Jackson')
+}
+
+function BIAN() {
+    alert('Thriller')
+}
